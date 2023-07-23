c8dd8c9d8268b65ad260a4095542d50811ed373e
Robert Gleason <l2ob122@gmail.com>
2023-07-22 20:32:40 -0700

    response headers

--- ASP/FirstApplication/Program.cs
+++ ASP/FirstApplication/Program.cs
@@ -3,4 +3,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Welcome to my first ASP.NET Core application!");
 
+
+app.Run(async (HttpContext context) =>
+{
+
+  context.Response.Headers["MyKey"] = "my value";
+  context.Response.Headers["Server"] = "My Server";
+  context.Response.Headers["Content-Type"] = "text/html";
+
+
+  await context.Response.WriteAsync("<h1>Welcome</h1");
+});
 app.Run();
